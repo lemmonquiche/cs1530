c189c4a35819d0527e1b14633fa45b037131a376
Maya Lemmon-Kishi <lemmonquiche@gmail.com>
2018-03-27 22:54:27 -0400

    Scheduler updates

--- scheduler.py
+++ scheduler.py
@@ -31,39 +31,81 @@ def gen_groups(course_id):
 
     #generate groups
     query = text("""SELECT max(group_id) FROM group""")
-    group_id = db.execute(query) + 1
-    #change
+    group_id = db.execute(query)
+    groups = []
+
+    #random list of values for possible times
     times = [[i] for i in range(196)]
     shuffle(times)
 
+    #for each groupable time
     for t in times:
+        #break if there is not enough students left to form a group
         if len(ss) < 5:
             break
+
+        #check if there is enough students to create a group at a given time
         count = sumColumn(sched_matrix, t)
+
+        #if groupable
         if count >= 5:
-            i = 0
+            #generate new group information, append to groups, and add to database
+            group_id += 1
+            groups.append(group_id)
             group = ({"group_id":group_id, "course_id":course_id})
             statement = text("""INSERT INTO group VALUES(:group_id, :course_id)""")
             db.execute(statement)
-            while i < 5:
-                if check((0, t)) == 1:
-                    stud = next(ss)
-                    #add to group_membership
+
+            #initialize values needed to create groups
+            i = 0
+            snum = 0
+            while i < 5 and snum < len(ss):
+                #if student is available at time
+                if sched_matrix[snum, t] == 1:
+                    #get student id: add to database that student is in this group
+                    stud = ss[snum]
                     data = ({"group_id":group_id, "student_id":stud})
                     statement = text("""INSERT INTO group_membership VALUES(:group_id, :course_id)""")
                     db.execute(statement)
-                    #remove students in group from ss
+
+                    #remove student from ss and their schedule from sched_matrix
                     ss.remove(stud)
                     np.delete(sched_matrix, t, 0)
+                    i += 1
                 else:
-                    next(ss)
+                    #if student not groupable, go to next student
+                    snum += 1
+
+    #if there are students leftover
+    while ss:
+        if len(ss) >= 5:
+            #just make these students into a group
+            #generate new group information, append to groups, and add to database
             group_id += 1
-
-
-    if not ss:
-        for stud in ss:
-            group_num.randint(1, group_id)
-            data = ({"group_id":group_num, "student_id":stud})
-            statement = text("""INSERT INTO group_membership VALUES(:group_id, :course_id)""")
+            groups.append(group_id)
+            group = ({"group_id":group_id, "course_id":course_id})
+            statement = text("""INSERT INTO group VALUES(:group_id, :course_id)""")
             db.execute(statement)
-            #somehow indicate group might not be optimal
+
+            #generate the group with first 5 students
+            for x in range(0, 5):
+                stud = ss[x]
+                data = ({"group_id":group_id, "student_id":stud})
+                statement = text("""INSERT INTO group_membership VALUES(:group_id, :course_id)""")
+                db.execute(statement)
+
+                #remove student from ss and their schedule from sched_matrix
+                ss.remove(stud)
+                np.delete(sched_matrix, t, 0)
+
+        else:
+            #if there is not enough students to form a group
+            for stud in ss:
+                num.randint(0, len(groups))
+                group_num = groups[num]
+                data = ({"group_id":group_num, "student_id":stud})
+                statement = text("""INSERT INTO group_membership VALUES(:group_id, :course_id)""")
+                db.execute(statement)
+                #somehow indicate group might not be optimal
+
+    return groups
