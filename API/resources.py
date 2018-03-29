b77f776c9d0ed7f4aeb6498fd8b33633de71b490
Maya Lemmon-Kishi <lemmonquiche@gmail.com>
2018-03-29 00:38:32 -0400

    debugging stuff

--- API/resources.py
+++ API/resources.py
@@ -3,9 +3,9 @@ from flask import g, session, jsonify
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 from ..models.models import Student, Instructor, RevokedTokenModel
 from ..scheduler import *
-from sqlalchemy.sql import text
-import json
+from sqlalchemy.sql import
 from __future__ import print_function # In python 2.7
+import json
 import sys
 
 parser = reqparse.RequestParser()
