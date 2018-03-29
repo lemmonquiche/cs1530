2ad302e04850da6d98da4f816f4ecdc6694e6dfb
Maya Lemmon-Kishi <lemmonquiche@gmail.com>
2018-03-29 11:54:18 -0400

    debugging stuff

--- python_scheduler_test.py
+++ python_scheduler_test.py
@@ -29,7 +29,8 @@ def gen_groups(course_id):
     for s in ss:
         #not sure if this query is formated correctly...
         sched = con.execute('SELECT available_hour_week FROM schedule WHERE student_id = :stud', {'stud':s})
-        print(sched)
+        for l in sched:
+            print(l)
         scheds = np.asarray(sched)
         print(scheds.shape)
         sched_matrix = np.concatenate((sched_matrix, sched), axis = 0)
