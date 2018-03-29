8ffad9f5f54082bc83bfdef21044371b0f8473f9
Maya Lemmon-Kishi <lemmonquiche@gmail.com>
2018-03-29 11:51:54 -0400

    debugging stuff

--- python_scheduler_test.py
+++ python_scheduler_test.py
@@ -28,9 +28,10 @@ def gen_groups(course_id):
     #get schedules for all students
     for s in ss:
         #not sure if this query is formated correctly...
-        sched = Schedule.bitstring_to_matrix(con.execute('SELECT available_hour_week FROM schedule WHERE student_id = :stud', {'stud':str(s)}))
+        sched = Schedule.bitstring_to_matrix(con.execute('SELECT available_hour_week FROM schedule WHERE student_id = :stud', {'stud':s}))
+        print(sched)
         scheds = np.asarray(sched)
-        print(sched.shape)
+        print(scheds.shape)
         sched_matrix = np.concatenate((sched_matrix, sched), axis = 0)
 
     #generate groups
