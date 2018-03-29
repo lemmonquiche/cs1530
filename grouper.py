bada4363af51a1a28cad06ba270573d113c97e0b
Craig Martin <craigmartin557@gmail.com>
2018-03-28 22:23:59 -0400

    added API endpoints

--- grouper.py
+++ grouper.py
@@ -81,6 +81,11 @@ api.add_resource(resources.LoginUser, '/api/login/user')
 api.add_resource(resources.LoginCridentials, '/api/login/credentials')
 api.add_resource(resources.GroupGenerate, '/api/group')
 api.add_resource(resources.AddSchedule, '/api/schedule/add')
+api.add_resource(resources.StudentDashBoard, '/api/studentdashboard')
+api.add_resource(resources.InstructorDashBoard, '/api/instructordashboard')
+api.add_resource(resources.RegisterForCourse, '/api/registerforcourse')
+api.add_resource(resources.ConfirmCourse, '/api/confirmstudent')
+api.add_resource(resource.CreateCourse, '/api/createcourse')
 
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
