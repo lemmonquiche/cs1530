cd6706e76ddaee9a8438ca35a1314885e734d4c4
Maya Lemmon-Kishi <lemmonquiche@gmail.com>
2018-03-29 11:11:04 -0400

    debugging stuff

--- python_scheduler_test.py
+++ python_scheduler_test.py
@@ -15,7 +15,7 @@ def sumColumn(m, column):
 def gen_groups(course_id):
     con = engine.connect()
     #get all students from course
-    students = con.execute('SELECT student_id FROM course_registration WHERE course_id = :course', {'course':course_id})
+    result = con.execute('SELECT student_id FROM course_registration WHERE course_id = :course', {'course':course_id})
 
     result = [r for r, in result]
 
