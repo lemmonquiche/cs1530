3af39ed2eab92d8c2723870b80b8b02a76515f1e
Maya Lemmon-Kishi <lemmonquiche@gmail.com>
2018-03-29 17:10:17 -0400

    this stupid scheduler

--- python_scheduler_test.py
+++ python_scheduler_test.py
@@ -29,8 +29,8 @@ def gen_groups(course_id):
 #    con = engine.connect()
     for s in ss:
 
-        result = con.execute('SELECT student_id FROM course_registration WHERE course_id = :course', {'course':course_id})
-        #result = con.execute('SELECT available_hour_week FROM schedule WHERE schedule_id = 1')
+#        result = con.execute('SELECT student_id FROM course_registration WHERE course_id = :course', {'course':course_id})
+        result = con.execute('SELECT available_hour_week FROM schedule WHERE schedule_id = :st', {'st':s})
 #        for r in result:
 #            print(r)
         sched = [r for (r, ) in result]
