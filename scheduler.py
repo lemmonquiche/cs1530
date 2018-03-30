9c70758df1b2f63fd9f71ffe519f2523215f013b
Daler Rahimov <daler.edu@gmail.com>
2018-03-29 21:30:40 -0400

    moved test to scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,13 @@
+from models.models import Course_Registration, Schedule, Group
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import random
 from random import shuffle
 import numpy as np
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-from models.models import db,  Course_Registration, Student, Schedule, Course, Group
-from sqlalchemy.sql import text
+engine = create_engine('sqlite:///grouper.db')
+
 
 def sumColumn(m, column):
     total = 0
@@ -14,24 +16,36 @@ def sumColumn(m, column):
     return total
 
 def gen_groups(course_id):
+    con = engine.connect()
     #get all students from course
-    students = db.session.query(Course_Registration).filter(Course_Registration.course_id == course_id)
+    result = con.execute('SELECT student_id FROM course_registration WHERE course_id = :course', {'course':course_id})
 
-    #make dict
-    ss = []
-    sched_matrix = []
+    ss = [r for (r, ) in result]
+
+    sched_matrix = np.empty((0, 196), int)
+    #print(sched_matrix.shape)
 
     #get schedules for all students
-    for s in students:
-        ss.append(s.student_id)
-        #not sure if this query is formated correctly...
-        sched = Schedule.bitstring_to_matrix(db.session.query(Schedule).filter(Schedule.student_id == s.student_id))
-        sched_matrix.append(sched)
-
+    con.close()
+    con = engine.connect()
+    for s in ss:
+        result = con.execute('SELECT available_hour_week FROM schedule WHERE schedule_id = :st', {'st':s})
+        sched = [r for (r, ) in result]
+        if not sched:
+            con.close()
+            return
+        sch = np.array(map(int, sched[0]))
+        sched_matrix = np.vstack((sched_matrix, sch))
 
     #generate groups
-    query = text("""SELECT max(group_id) FROM 'group' group by course""")
-    group_id = db.engine.execute(query)
+    gid = con.execute('SELECT max(group_id) FROM \'group\'')
+    group_i = [r for (r, ) in gid]
+    if group_i[0] is None:
+        group_id = 0
+    else:
+        group_id = int(group_i[0])
+
+    #if no gid is return, start at 0
     groups = []
 
     #random list of values for possible times
@@ -52,9 +66,7 @@ def gen_groups(course_id):
             #generate new group information, append to groups, and add to database
             group_id += 1
             groups.append(group_id)
-            group = ({"group_id":group_id, "course_id":course_id})
-            statement = text("""INSERT INTO 'group' VALUES(:group_id, :course_id)""")
-            db.engine.execute(statement)
+            con.execute('INSERT INTO \'group\' VALUES(:group, :course)', {'group':group_id, 'course':course_id})
 
             #initialize values needed to create groups
             i = 0
@@ -64,9 +76,7 @@ def gen_groups(course_id):
                 if sched_matrix[snum, t] == 1:
                     #get student id: add to database that student is in this group
                     stud = ss[snum]
-                    data = ({"group_id":group_id, "student_id":stud})
-                    statement = text("""INSERT INTO group_membership VALUES(:group_id, :student_id)""")
-                    db.engineself.execute(statement)
+                    con.execute('INSERT INTO group_membership(student_id, group_id, randomized) VALUES(:student_id, :group_id, :randomized)', {'student_id':stud, 'group_id':group_id, 'randomized':0})
 
                     #remove student from ss and their schedule from sched_matrix
                     ss.remove(stud)
@@ -83,28 +93,24 @@ def gen_groups(course_id):
             #generate new group information, append to groups, and add to database
             group_id += 1
             groups.append(group_id)
-            group = ({"group_id":group_id, "course_id":course_id})
-            statement = text("""INSERT INTO 'group' VALUES(:group_id, :course_id)""")
-            db.engine.execute(statement)
+            con.execute('INSERT INTO \'group\' VALUES(:group, :course)', {'group':group_id, 'course':course_id})
 
             #generate the group with first 5 students
             for x in range(0, 5):
                 stud = ss[x]
-                data = ({"group_id":group_id, "student_id":stud})
-                statement = text("""INSERT INTO group_membership VALUES(:group_id, :student_id)""")
-                db.engine.execute(statement)
-
+                con.execute('INSERT INTO group_membership(student_id, group_id, randomized) VALUES(:student_id, :group_id, :randomized)', {'student_id':stud, 'group_id':group_id, 'randomized':1})
                 #remove student from ss and their schedule from sched_matrix
                 ss.remove(stud)
-                np.delete(sched_matrix, t, 0)
         else:
             #if there is not enough students to form a group
             for stud in ss:
-                num.randint(0, len(groups))
+                g = len(groups) - 1
+                num = random.randint(0, g)
                 group_num = groups[num]
-                data = ({"group_id":group_num, "student_id":stud})
-                statement = text("""INSERT INTO group_membership VALUES(:group_id, :student_id)""")
-                db.engine.execute(statement)
+                con.execute('INSERT INTO group_membership(student_id, group_id, randomized) VALUES(:student_id, :group_id, :randomized)', {'student_id':stud, 'group_id':group_id, 'randomized':1})
+                ss.remove(stud)
                 #somehow indicate group might not be optimal
-
+    con.close()
+    for g in groups:
+        print("group id: ", g)
     return groups
