a818348018659689d0c6546b6df5245327a14047
Dave Ankin <alexanderankin@gmail.com>
2018-04-19 02:23:23 -0400

    clean up code

--- instructor/src/Components/Group.js
+++ instructor/src/Components/Group.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
 
 class Group extends Component {
-  // constructor() {
-  //   super(props);
-
-  //   this.state;
-  // }
   render() {
-    /*return <div style={{ border: '2px solid black' }}>
-      <p>Group Number: {this.props.group.id}</p>
-      <pre>{JSON.stringify(this.props.group, null, 2)}</pre>
-    </div>;*/
-
     return <div className="card bg-light mb-3 h-100">
       <div className="card-header">Group Number: {this.props.group.id}</div>
       <div className="card-body">
-        {/*<h5 className="card-title">Light card title</h5>*/}
-        {/*<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>*/}
         <div>
           <table className="table table-hover">
             <thead>
