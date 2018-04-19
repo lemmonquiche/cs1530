c2a6eb61114a7492b97bdbe10b24d4ae83385303
Dave Ankin <alexanderankin@gmail.com>
2018-04-19 00:48:51 -0400

    clean up App.js

--- instructor/src/App.js
+++ instructor/src/App.js
@@ -12,16 +12,9 @@ import Sidebar from './Components/nav/Sidebar';
 import routes from './routes';
 
 class App extends Component {
-  componentDidMount () {
-    var $this = $(ReactDOM.findDOMNode(this));
-    window.thing = $this.find('.full-height');
-    // window.thing.height($(window).height() - 66);
-  }
-
   render() {
     return <div>
       <div id="page-top"></div>
-      {/*<!-- Navigation -->*/}
       <Header />
       <div className="container-fluid" style={{
         padding: 0
@@ -31,14 +24,14 @@ class App extends Component {
           marginLeft: 0,
           marginRight: 0
         }}>
-          <div className="full-height col-md-2" style={{/*{ paddingRight: 0 }*/}}>
+          <div className="full-height col-md-2">
             <div style={{ margin: 10 }}><Sidebar active="ipsum" /></div>
           </div>
-          <div className="full-height col-md-10" style={{/*{ paddingLeft: 0 }*/}}>
+          <div className="full-height col-md-10">
             <div style={{ margin: 10 }}>
-              <Route path="/classes" component={routes.Classes}/>
-              <Route path="/profile" component={routes.Profile}/>
-              <Route path="/help" component={routes.Help}/>
+              <Route path="/classes" component={routes.Classes} />
+              <Route path="/profile" component={routes.Profile} />
+              <Route path="/help"    component={routes.Help} />
             </div>
           </div>
         </div>
